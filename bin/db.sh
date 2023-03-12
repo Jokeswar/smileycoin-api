6e741dd76b6644aa2187a21eec2d0f3ec3e3de33
Mihai Baruta <baruta.mihai99@gmail.com>
2023-03-12 17:02:42 +0200

    Add Docker entrypoint

--- bin/db.sh
+++ bin/db.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 docker run --name mongo \
-            -p 27017:27017 \
+            -p "${MONGODB_PORT}":27017 \
             -e "MONGO_INITDB_ROOT_USERNAME=${MONGO_INITDB_ROOT_USERNAME}" \
             -e "MONGO_INITDB_ROOT_PASSWORD=${MONGO_INITDB_ROOT_USERNAME}" \
             -d \
