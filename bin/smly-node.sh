37730bf9e0c2c3cce295925fd247ec6f4be3af58
Mihai Baruta <baruta.mihai99@gmail.com>
2023-03-21 23:56:58 +0200

    Customize the smiley node launcher script

--- bin/smly-node.sh
+++ bin/smly-node.sh
@@ -1,26 +1,35 @@
 #!/bin/bash
 
 function random_port {
-    local CHECK="do while"
+    local check="do while"
 
-    while [[ ! -z $CHECK ]]; do
-        local PORT=$(( ( RANDOM % 60000 )  + 1025 ))
-        CHECK=$(sudo netstat -ap | grep $PORT)
+    while [[ -n "$check" ]]; do
+        local port=$(( ( RANDOM % 60000 )  + 1025 ))
+        check=$(sudo netstat -ap | grep "$port")
     done
 
-    echo $PORT
+    echo "$port"
 }
 
-SMILEY_COIN_ID="$(date +%s)"
-CONTAINER_NAME="smileycoin-node-${SMILEY_COIN_ID}"
+SMILEYCOIN_ID="$(date +%s)"
+CONTAINER_NAME="smileycoin-node-${SMILEYCOIN_ID}${SMILEYCOIN_SUFFIX}"
 VOLUME_NAME="$CONTAINER_NAME-data"
 HOST_PORT="$(random_port)"
 
-docker volume create "$VOLUME_NAME"
+docker volume create "$VOLUME_NAME" > /dev/null
 
 docker run \
     -d \
     -p "$HOST_PORT":9332 \
     --mount source="$VOLUME_NAME",destination=/smly-data\
     --name "$CONTAINER_NAME" \
-    smileycoin-node
+    smileycoin-node > /dev/null
+
+echo ""
+echo "INFO:"
+echo ""
+echo "Container name: $CONTAINER_NAME"
+echo "Volume name:    $VOLUME_NAME"
+echo "Host Port:      $HOST_PORT"
+docker exec smileycoin-node-1679382496 /bin/bash -c 'cat /smly-data/smileycoin.conf' | head -2
+echo ""
